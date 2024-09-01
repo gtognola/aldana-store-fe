bd8b51a5a115b832f4c2f23f93ceeaf1c7877f4f
ftogno92 <tognofacundo0@gmail.com>
2024-09-01 05:10:48 -0300

    [FEAT] creation of the details product component

--- src/app/product/[id]/page.jsx
+++ src/app/product/[id]/page.jsx
@@ -1,6 +1,6 @@
 "use client"
 import { useState, useEffect } from 'react'
-import Image from 'next/image';
+import ProductDetail from '/home/ftognola/aldana-store-fe/src/components/details'
 
 export default function Page({ params }) {
 
@@ -22,32 +22,11 @@ export default function Page({ params }) {
                 console.log('Error:', error)
             })
     }, [])
-
+    if (isLoading) return <p>Cargando...</p>;
     return (
-        <div className='container '>
+        <div className='container'>
 
-            {product ? (
-                <>
-                    <div className='grid grid-cols-2 my-5 gap-6'>
-                        <div className='grid place-content-end'>
-                            <h1 className='grid place-content-center text-xl my-5'>{product.name}</h1>
-                            <Image src={product.image} alt={product.name} width={400} height={400} className='rounded-xl shadow-2xl'/>
-                        </div>
-                        <div className="grid place-content-center px-2 py-4 gap-6">
-                            <p>{product.description}</p>
-                            <p>${product.price}</p>
-                            <div className='my-5'>
-                                <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-                                    Add to Cart
-                                </button>
-                            </div>
-                        </div>
-                    </div>
-                </>
-            ) : (
-                <p>Producto no encontrado</p>
-            )
-            }
+            <ProductDetail product={product}></ProductDetail>
 
         </div >
 
